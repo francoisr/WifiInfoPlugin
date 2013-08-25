8293c7139de0fdd9fc67857a3ca8f36f82780141
Francois Rioux <frioux1@gmail.com>
2013-08-25 21:55:47 +0200

    Replacing all code, renaming only var geolocation to wifiinfo

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -1,18 +1,23 @@
-var argscheck = require('cordova/argscheck'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec');
-
-//var wifiinfo = {
-//    lastPosition:null
-////    , // reference to last known (cached) position returned
-////    test:function(){alert('in wifiiinfo.test, // reference to last known (cached) position returned')},
-//////    getWifiInfo:function(success, fail) {
-////        alert('calling : exec(success, fail, "WifiInfoPlugin", null, [] );')
-////		exec(success, fail, 'WifiInfoPlugin', null, [] );
-////	    alert('called : exec(success, fail, ·WifiInfoPlugin", null, [] );')
-////		}
-//	};
-
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
 
 var argscheck = require('cordova/argscheck'),
     utils = require('cordova/utils'),
