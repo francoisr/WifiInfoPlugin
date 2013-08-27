6f118190269a450e05f2456d4f4d77c00efeca90
Francois Rioux <frioux1@gmail.com>
2013-08-27 06:06:17 +0200

    Just wifiinfo.  original id

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -4,13 +4,13 @@ var argscheck = require('cordova/argscheck'),
 
 var wifiinfo = {
     lastPosition:null
-//    , // reference to last known (cached) position returned
-//    test:function(){alert('in wifiiinfo.test, // reference to last known (cached) position returned')},
-////    getWifiInfo:function(success, fail) {
-//        alert('calling : exec(success, fail, "WifiInfoPlugin", null, [] );')
-//		exec(success, fail, 'WifiInfoPlugin', null, [] );
-//	    alert('called : exec(success, fail, ·WifiInfoPlugin", null, [] );')
-//		}
+    , // reference to last known (cached) position returned
+    test:function(){alert('in wifiiinfo.test, // reference to last known (cached) position returned')},
+    getWifiInfo:function(success, fail) {
+        alert('calling : exec(success, fail, "WifiInfoPlugin", null, [] );')
+		exec(success, fail, 'WifiInfoPlugin', null, [] );
+	    alert('called : exec(success, fail, ·WifiInfoPlugin", null, [] );')
+		}
 	};
 
 module.exports = wifiinfo;
