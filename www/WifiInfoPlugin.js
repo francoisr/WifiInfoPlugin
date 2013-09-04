a1ccd4e13157b2733ee01360f5fd17b4b571b6d9
Francois Rioux <frioux1@gmail.com>
2013-09-04 06:52:49 +0200

    calling callback

--- www/WifiInfoPlugin.js
+++ www/WifiInfoPlugin.js
@@ -6,11 +6,17 @@ var wifiinfo = {
     lastPosition:null
     , // reference to last known (cached) position returned
     test:function(){alert('in wifiiinfo.test, // reference to last known (cached) position returned')},
-    getWifiInfo:function(success, fail) {
-//        alert('calling : exec(success, fail, "WifiInfoPlugin", null, [] );')
-		exec(success, fail, 'WifiInfoPlugin', null, [] );
-//	    alert('called : exec(success, fail, "WifiInfoPlugin", null, [] );')
-		}
+    getWifiInfo: function (successCallback, errorCallback) {
+		exec(win, fail, 'WifiInfoPlugin', null, [] );
+
+		var win = function (p) {
+		    successCallback(pos);
+            };
+
+		var fail = function (p) {
+		    errorCallback(pos);
+            };
+        }
 	};
 
 module.exports = wifiinfo;
